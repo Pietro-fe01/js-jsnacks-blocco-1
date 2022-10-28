3ec2312a6b904379e22858e66d7903aed99ec9fd
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-10-28 10:21:44 +0200

    Completato snack 6

--- jsnack5/js/script.js
+++ jsnack5/js/script.js
@@ -1,10 +1,12 @@
+"use strict";
+
 /*Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.*/
 
 const list = [];
 
-for (i=1; i<=6; i++){
+for (let i=1; i<=6; i++){
     let num = Number(prompt("Inserisci un numero:"));
     if (num % 2 != 0){
         list.push(num);
